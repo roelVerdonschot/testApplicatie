4dda6ddc34ecc85c4ee32f05cc9b4d89c2836d1e
Roel <roel_verdonschot@hotmail.com>
2016-03-15 10:03:09 +0100

    db settings circle

--- app/config/database.php
+++ app/config/database.php
@@ -82,6 +82,17 @@ return array(
 			'password' => '',
 			'prefix'   => '',
 		),
+		
+		'circle' => array(
+			'driver'    => 'mysql',
+			'host'      => '127.0.01',
+			'database'  => 'circle_test',
+			'username'  => 'ubuntu',
+			'password'  => '',
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+		),
 
 	),
 
